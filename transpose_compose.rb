bef637ebda2cc7021cc1f3845cbefab08b94f8db
Luke van der Hoeven <hi@plukevdh.me>
2016-11-22 14:54:40 -0500

    docker compose transposition script

---
+++ transpose_compose.rb
@@ -0,0 +1,35 @@
+#!/usr/bin/ruby
+require 'yaml'
+
+DEFAULT_MEM_LIMIT = "256M"
+
+image_name = ARGV[0]
+build_name = ARGV[1]
+
+def detect_command(service)
+  command = service["command"]
+  command ||= `cat Dockerfile | grep CMD | sed 's/CMD //'`.strip
+
+  "./pr-start.sh #{command}"
+end
+
+compose = YAML.load_file('./data/docker-compose.yml')
+compose["services"].each do |service_name, service|
+  service.delete("labels")
+
+  service["image"] = image_name if service.delete("build")
+  service["mem_limit"] ||= DEFAULT_MEM_LIMIT
+
+  if service_name == "app"
+    service["command"] = detect_command(service)
+
+    service["environment"] << "SERVICE_3000_CHECK_INTERVAL=15s"
+    service["environment"] << "SERVICE_3000_CHECK_TCP=true"
+    service["environment"] << "SERVICE_3000_NAME=#{build_name}"
+    service["environment"] << "SERVICE_3000_TAGS=urlprefix-#{build_name}.peer.articulate.zone/"
+  end
+
+  compose["services"][service_name] = service
+end
+
+puts compose.to_yaml
