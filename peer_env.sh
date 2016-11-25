5eafa617e1fc464424dfb2921f22d1d4c982dec3
Luke van der Hoeven <hi@plukevdh.me>
2016-11-24 21:42:13 -0500

    Create peer_env.sh

---
+++ peer_env.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+if [ ${PEER_CONSUL_ADDR} ]
+then
+  if consul-template -consul=$PEER_CONSUL_ADDR -template=peer_exports.ctmpl:/tmp/peer_exports.sh -once
+  then
+    source /tmp/peer_exports.sh
+  else
+    echo "======== COULD NOT LOAD PEER VARS ========"
+    exit 1
+  fi
+else
+  echo "PEER_CONSUL_ADDR is not set skipping peer-specific exports"
+fi
